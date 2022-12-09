86fdc4f15f9b79549aa0cdb4edb3523a2d01bd29
Marius <marius.ihring@icloud.com>
2022-12-09 13:15:39 +0000

    seeding db
    - new bug ITS-18
    - new bug ITS-17

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,67 @@
-import { prisma } from "../src/server/db/client"
-
+import { prisma } from "../src/server/db/client";
 
 async function main() {
-    
-} 
+  await prisma.userData.upsert({
+    where: { mail: "test@test.de" },
+    update: {},
+    create: {
+      mail: "test@test.de",
+      lastName: "Bayindir",
+      firstName: "Pascal",
+      interests: {
+        create: {
+          webDevelopment: true,
+          cyberSecurity: false,
+          mobileDev: true,
+          design: true,
+          dataScience: false,
+          coding: true,
+        },
+      },
+      picture: "abcd",
+      company: {
+        create: {
+          isAssociated: true,
+          companyName: "Bosch",
+          companyEmail: "pascal@bosch.com",
+        },
+      },
+    },
+  });
+  await prisma.userData.upsert({
+    where: { mail: "test2@test.de" },
+    update: {},
+    create: {
+      mail: "test2@test.de",
+      lastName: "Ihring",
+      firstName: "Marius",
+      interests: {
+        create: {
+          webDevelopment: true,
+          cyberSecurity: false,
+          mobileDev: true,
+          design: true,
+          dataScience: false,
+          coding: true,
+        },
+      },
+      picture: "abcd",
+      company: {
+        create: {
+          isAssociated: true,
+          companyName: "Bosch",
+          companyEmail: "marius@bosch.com",
+        },
+      },
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
