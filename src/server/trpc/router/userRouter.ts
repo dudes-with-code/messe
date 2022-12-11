96ee3f25099e02419e56bde77b0fae6743eff74e
Marius <marius.ihring@icloud.com>
2022-12-11 19:13:06 +0100

    added updateUser API Endpoint

--- src/server/trpc/router/userRouter.ts
+++ src/server/trpc/router/userRouter.ts
@@ -58,4 +58,55 @@ export const userRouter = router({
     .mutation(({ ctx, input }) => {
       return ctx.prisma.userData.findFirst({ where: { mail: input.mail } });
     }),
+  changeSpecificUserData: publicProcedure
+    .input(
+      z.object({
+        mail: z.string(),
+        lastName: z.string(),
+        firstName: z.string(),
+        picture: z.string(),
+        interests: z.object({
+          webDevelopment: z.boolean(),
+          cyberSecurity: z.boolean(),
+          mobileDev: z.boolean(),
+          design: z.boolean(),
+          dataScience: z.boolean(),
+          coding: z.boolean(),
+        }),
+        company: z.object({
+          isAssociated: z.boolean(),
+          companyEmail: z.string(),
+          companyName: z.string(),
+        }),
+      })
+    )
+      .mutation(({ctx, input}) => {
+        return ctx.prisma.userData.update({
+          where: {mail: input.mail},
+          data: {
+            mail: input.mail,
+          lastName: input.lastName,
+          firstName: input.firstName,
+          picture: input.picture,
+          interests: {
+            create: {
+              webDevelopment: input.interests.webDevelopment,
+              cyberSecurity: input.interests.cyberSecurity,
+              mobileDev: input.interests.mobileDev,
+              design: input.interests.design,
+              dataScience: input.interests.dataScience,
+              coding: input.interests.coding,
+            },
+          },
+          company: {
+            create: {
+              isAssociated: input.company.isAssociated,
+              companyEmail: input.company.companyEmail,
+              companyName: input.company.companyName,
+            },
+          },
+          }
+        })
+      })
+  
 });
