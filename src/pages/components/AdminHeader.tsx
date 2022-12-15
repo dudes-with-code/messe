e1eb77fc7e39a2ebd8f6b5945619e9471b69138a
Marius Ihring <marius.ihring@icloud.com>
2022-12-15 14:35:57 +0100

    header test

--- src/pages/components/AdminHeader.tsx
+++ src/pages/components/AdminHeader.tsx
@@ -1,6 +1,6 @@
 export default function AdminHeader() {
     return (
-        <div>
+        <div className="w-full bg-gray-500 text-white flex items-center">
             <p>Admin Header</p>
         </div>
     )
