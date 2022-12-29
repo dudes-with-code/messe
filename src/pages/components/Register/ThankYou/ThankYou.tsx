e4d75d8dc1e42cd88cf95fcc5e9e16ac938622e0
Marius Ihring <marius.ihring@icloud.com>
2022-12-30 00:19:57 +0100

    added thank you screen

--- src/pages/components/Register/ThankYou/ThankYou.tsx
+++ src/pages/components/Register/ThankYou/ThankYou.tsx
@@ -1,3 +1,18 @@
+import { useContext } from "react"
+import { UserContext } from "../../../context/userDataContext"
+
 export default function ThankYou() {
-  return <div>Thank You</div>;
+  const { user, setUser } = useContext(UserContext)
+  return (
+    <div>
+      <h1 className="align-center mb-18 h-28 justify-center pt-44 text-4xl font-bold text-slate-200">
+        Thank You <br />
+        {user.firstName}!
+      </h1>
+
+      <div className="mt-44 flex flex-wrap items-center justify-center gap-3.5">
+        Here downloadable ticket loool
+      </div>
+    </div>
+  )
 }
