b3a3363b29db87a08bc53d27952c362d0e51a20b
Marius <marius.ihring@icloud.com>
2023-01-07 22:05:34 +0100

    fixed api responeslimit

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -15,6 +15,9 @@ export const trpc = createTRPCNext<AppRouter>({
   config() {
     return {
       transformer: superjson,
+      api: {
+        responseLimit: false,
+      },
       links: [
         loggerLink({
           enabled: (opts) =>
